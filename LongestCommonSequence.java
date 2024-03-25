345a0dcd1d9a7ca366c684a2e811e94cb2a57d81
huckleberri6 <j-devore.1@onu.edu>
2024-03-25 10:53:16 -0400

    idk if this works :)

--- LongestCommonSequence.java
+++ LongestCommonSequence.java
@@ -2,11 +2,24 @@ import java.util.*;
 
 public class LongestCommonSequence {
 
-    public LongestCommonSequence(String a, String b, String c) {
+    public LongestCommonSequence() {
+    }
 
+    public int LCS(String a, String b) {
+        int count = 0;
+
+        for (int i = 0; i < a.length(); i++) {
+            if (b.contains(a.substring(i, i + 1)))
+                count++;
+        }
+
+        return count;
     }
 
     public int main() {
-        return 0;
+        String a = "abc";
+        String b = "ac";
+        System.out.println(LCS(a, b));
+        return LCS(a, b);
     }
 }
