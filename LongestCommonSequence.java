5e00c03467b014df46d3bbb2c424b5954458dc0c
huckleberri6 <j-devore.1@onu.edu>
2024-03-26 20:21:49 -0400

    done but may need more comments

--- LongestCommonSequence.java
+++ LongestCommonSequence.java
@@ -1,25 +1,65 @@
 import java.util.*;
 
 public class LongestCommonSequence {
+    private static String lcs = "";
 
     public LongestCommonSequence() {
     }
 
     public static int LCS(String a, String b) {
-        int count = 0;
+        lcs = ""; // just re-initializing the longest common sequence
+        int count = 0; // variable to keep track of number of shared letters
+        String[] bLetters = new String[b.length()]; // this array stores all the letters in string b
 
+        // storing all the letters in string b into an array that
+        // will be used as a lookup
+        for (int i = 0; i < b.length(); i++)
+            bLetters[i] = b.substring(i, i + 1);
+
+        // finding the letters in string a that are also in string b
         for (int i = 0; i < a.length(); i++) {
-            if (b.contains(a.substring(i, i + 1)))
-                count++;
+            for (int j = 0; j < b.length(); j++)
+                if (bLetters[j].equals(a.substring(i, i + 1))) // the letter in a is also a letter in b
+                {
+                    count++;
+                    lcs = lcs + a.substring(i, i + 1);
+                }
         }
 
-        return count;
+        return count; // number of common letters between string a and string b
     }
 
     public static void main(String args[]) {
-        String a = "abc";
-        String b = "ac";
-        System.out.println(LCS(a, b));
-        // return LCS(a, b);
+        // the following test cases are the examples shown in class
+
+        String a = "abcde";
+        String b = "ace";
+        System.out.print(LCS(a, b)); // prints 3
+        System.out.println("  " + lcs);
+
+        a = "BD";
+        b = "AE";
+        System.out.print(LCS(a, b)); // prints 0
+        System.out.println("  " + lcs);
+
+        a = "abc";
+        b = "abc";
+        System.out.print(LCS(a, b)); // prints 3
+        System.out.println("  " + lcs);
+
+        a = "abc";
+        b = "ABC";
+        System.out.print(LCS(a, b)); // prints 0
+        System.out.println("  " + lcs);
+
+        a = "123";
+        b = "12345";
+        System.out.print(LCS(a, b)); // prints 3
+        System.out.println("  " + lcs);
+
+        a = ";/%-";
+        b = ";%";
+        System.out.print(LCS(a, b)); // prints 2
+        System.out.println("  " + lcs);
     }
 }
